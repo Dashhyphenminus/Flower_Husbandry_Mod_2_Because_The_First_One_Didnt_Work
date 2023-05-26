4d833f7942f71addea8df29b2c53898c1fbdca76
dashhyphenminus <robertblendea@outlook.com>
2023-05-26 10:26:48 -0700

    Made custom blocks a thing

---
+++ src/main/java/com/dashhy/flowerhusbandrymod/block/ModBlocks.java
@@ -0,0 +1,33 @@
+package com.dashhy.flowerhusbandrymod.block;
+
+import com.dashhy.flowerhusbandrymod.FlowerHusbandryMod;
+import com.dashhy.flowerhusbandrymod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FlowerHusbandryMod.MOD_ID);
+
+
+    private static <T extends Block> RegistryObject<T> RegisterBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
