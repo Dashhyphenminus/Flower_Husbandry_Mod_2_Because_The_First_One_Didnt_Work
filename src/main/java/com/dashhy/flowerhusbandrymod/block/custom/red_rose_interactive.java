d6791a71fdbc600f112b83b29b6ed8ee830aa087
dashhyphenminus <robertblendea@outlook.com>
2023-05-27 14:04:22 -0700

    roses!

---
+++ src/main/java/com/dashhy/flowerhusbandrymod/block/custom/red_rose_interactive.java
@@ -0,0 +1,11 @@
+package com.dashhy.flowerhusbandrymod.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class red_rose_interactive extends Block {
+
+
+    public red_rose_interactive(Properties properties) {
+        super(properties);
+    }
+}
