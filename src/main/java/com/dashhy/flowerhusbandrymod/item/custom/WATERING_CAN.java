d836826e0344186841616d8665827443042be525
legit_q9yt <legitbread111@gmail.com>
2023-05-29 10:50:56 -0700

    hi

---
+++ src/main/java/com/dashhy/flowerhusbandrymod/item/custom/WATERING_CAN.java
@@ -0,0 +1,16 @@
+package com.dashhy.flowerhusbandrymod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class WATERING_CAN  extends Item {
+    public WATERING_CAN(Properties properties) {
+        super(properties);
+    }
+/*
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        return super.use(level, player, hand);
+    }
+
+ */
+}
