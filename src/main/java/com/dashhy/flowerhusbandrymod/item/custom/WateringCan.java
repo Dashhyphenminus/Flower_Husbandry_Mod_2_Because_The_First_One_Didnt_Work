18acf8b6a0d5d17b5b05c7500cbbe48c791dc468
dashhy <robertblendea@outlook.com>
2023-06-07 17:32:51 -0700

    deleted red_rose_interactive.java

--- src/main/java/com/dashhy/flowerhusbandrymod/item/custom/WateringCan.java
+++ src/main/java/com/dashhy/flowerhusbandrymod/item/custom/WateringCan.java
@@ -115,77 +115,77 @@ public class WateringCan extends Item {
 
             search(level, blockPos, 0);
 
-            //if (first) {
-            newFlowerColorVal = 0;
-            if (blockState.getBlock().equals(WHITE_ROSE.get())) {
-                newFlowerColorVal += 0;
-            } else if (blockState.getBlock().equals(PINK_ROSE.get())) {
-                newFlowerColorVal += 1;
-            } else if (blockState.getBlock().equals(RED_ROSE.get())) {
-                newFlowerColorVal += 2;
-            } else if (blockState.getBlock().equals(ORANGE_ROSE.get())) {
-                newFlowerColorVal += 3;
-            } else if (blockState.getBlock().equals(YELLOW_ROSE.get())) {
-                newFlowerColorVal += 4;
-            } else if (blockState.getBlock().equals(RAINBOW_ROSE.get())) {
-                newFlowerColorVal += 5;
-            }
-
-
-            //} else {
-            BlockState autoFindBlock = search(level, blockPos, 0);
-
-            if (autoFindBlock == null) {
-                System.out.println("No flowers found");
-            } else {
-                if (autoFindBlock.getBlock().equals(WHITE_ROSE.get())) {
+            if (first) {
+                newFlowerColorVal = 0;
+                if (blockState.getBlock().equals(WHITE_ROSE.get())) {
                     newFlowerColorVal += 0;
-                } else if (autoFindBlock.getBlock().equals(PINK_ROSE.get())) {
+                } else if (blockState.getBlock().equals(PINK_ROSE.get())) {
                     newFlowerColorVal += 1;
-                } else if (autoFindBlock.getBlock().equals(RED_ROSE.get())) {
+                } else if (blockState.getBlock().equals(RED_ROSE.get())) {
                     newFlowerColorVal += 2;
-                } else if (autoFindBlock.getBlock().equals(ORANGE_ROSE.get())) {
+                } else if (blockState.getBlock().equals(ORANGE_ROSE.get())) {
                     newFlowerColorVal += 3;
-                } else if (autoFindBlock.getBlock().equals(YELLOW_ROSE.get())) {
+                } else if (blockState.getBlock().equals(YELLOW_ROSE.get())) {
                     newFlowerColorVal += 4;
-                } else if (autoFindBlock.getBlock().equals(RAINBOW_ROSE.get())) {
+                } else if (blockState.getBlock().equals(RAINBOW_ROSE.get())) {
                     newFlowerColorVal += 5;
+                }
+                first = false;
+
+            } else {
+
+
+                if (blockState == null) {
+                    System.out.println("No flowers found");
                 } else {
-                    System.out.println(autoFindBlock.getBlock());
+                    if (blockState.getBlock().equals(WHITE_ROSE.get())) {
+                        newFlowerColorVal += 0;
+                    } else if (blockState.getBlock().equals(PINK_ROSE.get())) {
+                        newFlowerColorVal += 1;
+                    } else if (blockState.getBlock().equals(RED_ROSE.get())) {
+                        newFlowerColorVal += 2;
+                    } else if (blockState.getBlock().equals(ORANGE_ROSE.get())) {
+                        newFlowerColorVal += 3;
+                    } else if (blockState.getBlock().equals(YELLOW_ROSE.get())) {
+                        newFlowerColorVal += 4;
+                    } else if (blockState.getBlock().equals(RAINBOW_ROSE.get())) {
+                        newFlowerColorVal += 5;
+                    } else {
+                        System.out.println(blockState.getBlock());
+                    }
+
+                    newFlowerColorVal /= 2;
+
+                    double[] rands = {Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5};
+                    Arrays.sort(rands);
+                    newFlowerColorVal += rands[1];
+
+                    if (Math.random() < 0.02) {
+                        ItemStack stack = new ItemStack(RAINBOW_ROSE.get());
+                        player.getInventory().add(stack);
+                    } else if (newFlowerColorVal < colorValues[1]) {
+                        ItemStack stack = new ItemStack(WHITE_ROSE.get());
+                        player.getInventory().add(stack);
+                    } else if (newFlowerColorVal < colorValues[2]) {
+                        ItemStack stack = new ItemStack(PINK_ROSE.get());
+                        player.getInventory().add(stack);
+                    } else if (newFlowerColorVal < colorValues[3]) {
+                        ItemStack stack = new ItemStack(RED_ROSE.get());
+                        player.getInventory().add(stack);
+                    } else if (newFlowerColorVal < colorValues[4]) {
+                        ItemStack stack = new ItemStack(ORANGE_ROSE.get());
+                        player.getInventory().add(stack);
+                    } else if (newFlowerColorVal < colorValues[5]) {
+                        ItemStack stack = new ItemStack(YELLOW_ROSE.get());
+                        player.getInventory().add(stack);
+                    } else {
+                        ItemStack stack = new ItemStack(RAINBOW_ROSE.get());
+                        player.getInventory().add(stack);
+                    }
                 }
 
-                newFlowerColorVal /= 2;
-
-                double[] rands = {Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5};
-                Arrays.sort(rands);
-                newFlowerColorVal += rands[1];
-
-                if (Math.random() < 0.02) {
-                    ItemStack stack = new ItemStack(RAINBOW_ROSE.get());
-                    player.getInventory().add(stack);
-                } else if (newFlowerColorVal < colorValues[1]) {
-                    ItemStack stack = new ItemStack(WHITE_ROSE.get());
-                    player.getInventory().add(stack);
-                } else if (newFlowerColorVal < colorValues[2]) {
-                    ItemStack stack = new ItemStack(PINK_ROSE.get());
-                    player.getInventory().add(stack);
-                } else if (newFlowerColorVal < colorValues[3]) {
-                    ItemStack stack = new ItemStack(RED_ROSE.get());
-                    player.getInventory().add(stack);
-                } else if (newFlowerColorVal < colorValues[4]) {
-                    ItemStack stack = new ItemStack(ORANGE_ROSE.get());
-                    player.getInventory().add(stack);
-                } else if (newFlowerColorVal < colorValues[5]) {
-                    ItemStack stack = new ItemStack(YELLOW_ROSE.get());
-                    player.getInventory().add(stack);
-                } else {
-                    ItemStack stack = new ItemStack(RAINBOW_ROSE.get());
-                    player.getInventory().add(stack);
-                }
+                first = true;
             }
-
-
-            //}
             idkWhyThisNeedsToExistButHereWeAre = true;
         } else {
             idkWhyThisNeedsToExistButHereWeAre = false;
